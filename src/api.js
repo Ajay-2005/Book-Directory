7458b3cd5faf25b38f73904a160615b032a73c7c
Ajay A <aji478514@gmail.com>
2025-07-07 11:28:04 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const books = require('./books_list');
 let BookDirectory = books;
-
+// test CI
 router.get('/books', (req, res) => {
     res.send(BookDirectory);
 });
